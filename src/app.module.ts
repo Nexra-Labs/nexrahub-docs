da6630caab861451ee4abba17909bc3e470241f6
Adaobi-Chuks <chuksaginamada@gmail.com>
2025-10-17 14:44:23 +0100

    Feat: create all needed modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app/app.service';
 import { AppController } from './app/app.controller';
+import { UserModule } from './user/user.module';
+import { GameModule } from './game/game.module';
+import { TournamentModule } from './tournament/tournament.module';
+import { BetModule } from './bet/bet.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { CommissionModule } from './commission/commission.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, GameModule, TournamentModule, BetModule, TransactionModule, CommissionModule],
   controllers: [AppController],
   providers: [AppService],
 })
