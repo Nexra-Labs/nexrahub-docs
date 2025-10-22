e7aef27b4ab36505b3b7a744e7477f5594df91a1
Adaobi-Chuks <chuksaginamada@gmail.com>
2025-10-22 16:29:29 +0100

    Feat: Add dev upgrade and user fetch endpoints

--- src/user/providers/user.service.ts
+++ src/user/providers/user.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { UserRepository } from '../repositories/user.repository';
 import { IUser } from '../interfaces/user.interface';
 import { generateUniqueUsername } from '../utils/username.utils';
+import { UserRole } from '../enums/user-role.enum';
+import { Types } from 'mongoose';
 
 @Injectable()
 export class UserService {
@@ -22,5 +24,23 @@ export class UserService {
         } while (await this.findByUsername(username));
 
         return this.userRepo.create({ email, username });
+    }
+
+    async upgradeToDeveloper(userId: string | Types.ObjectId): Promise<IUser> {
+        const user = await this.userRepo.findById(userId);
+        if (!user) throw new NotFoundException('User not found');
+
+        if (user.role === UserRole.DEVELOPER) {
+            throw new BadRequestException('User is already a developer');
+        }
+
+        user.role = UserRole.DEVELOPER;
+        await user.save();
+
+        return user;
+    }
+
+    async findUserById(userId: string): Promise<IUser | null> {
+        return this.userRepo.findById(userId);
     }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,64 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Patch,
+    HttpCode,
+    HttpStatus,
+    Req,
+    Get,
+} from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiSecurity } from '@nestjs/swagger';
+import { UserService } from './providers/user.service';
+import { ActiveUser } from 'src/auth/decorators/active-user.decorator';
+import { IUser } from './interfaces/user.interface';
 
+@ApiTags('User')
+@ApiSecurity('jwt-auth')
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(
+        private readonly userService: UserService
+    ) { }
+
+    @Patch('upgrade')
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: 'Upgrade user to developer account' })
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: 'User successfully upgraded to developer',
+        schema: {
+            example: {
+                message: 'User upgraded to developer successfully',
+                role: 'developer',
+            },
+        },
+    })
+    @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'User is already a developer' })
+    async upgradeToDeveloper(@ActiveUser() user: IUser) {
+        const result = await this.userService.upgradeToDeveloper(user.id);
+        return { message: 'User upgraded to developer successfully', role: result.role };
+    }
+
+    //fetch loggedin user
+    @Get('me')
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: 'Fetch loggedin user' })
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: 'Loggedin user data fetched successfully',
+        schema: {
+            example: {
+                "_id": "68f8d5eb71ee81dd785a2f6f",
+                "username": "frozen_moon_0",
+                "email": "chuksaginamada@gmail.com",
+                "role": "user",
+                "walletAddress": null,
+                "balance": 0,
+                "createdAt": "2025-10-22T13:02:35.951Z",
+                "updatedAt": "2025-10-22T13:02:35.951Z"
+            }
+        },
+    })
+    async me(@ActiveUser() user: IUser) {
+        return user;
+    }
+}
